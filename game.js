3738e5ffa83d87da12795b9b6049f03129e7d2f0
Nicholas Jaquez <nrjaquez@gmail.com>
2021-03-01 22:36:03 -0800

    JS file created

---
+++ game.js
@@ -0,0 +1,10 @@
+buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4)
+    // return randomNumber;
+};
+
+nextSequence();
+
+
